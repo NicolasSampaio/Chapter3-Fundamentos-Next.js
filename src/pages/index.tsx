cfd2e7d34a6f7133fdaf55d7d0e395a4ba20c98e
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2022-01-19 11:13:19 -0300

    Difference between serverSide and static site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
 
 import styles from "./home.module.scss";
+
+// Client-Side
+// Server-Side
+// Static Site Generation
 
 interface HomeProps {
   product: {
@@ -37,7 +41,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1KJeM3GtTDbhA9F5Qa2DLqeh", {
     expand: ["product"],
   });
@@ -54,5 +58,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, //24 hours
   };
 };
